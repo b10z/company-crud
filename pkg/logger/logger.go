3e913fca42fb631586b24206c571885f8a702e95
b10z <ngeorgedakis@gmail.com>
2024-12-14 04:45:45 +0200

    initial service scaffolding

---
+++ pkg/logger/logger.go
@@ -0,0 +1,36 @@
+package logger
+
+import (
+	"errors"
+	"go.uber.org/zap"
+)
+
+type Logger struct {
+	*zap.Logger
+}
+
+func New(env string) (*Logger, error) {
+	logger := zap.NewNop()
+
+	var err error
+	switch env {
+	case "stag":
+		fallthrough
+	case "dev":
+		logger, err = zap.NewDevelopment()
+		if err != nil {
+			return nil, err
+		}
+
+	case "prod":
+		logger, err = zap.NewProduction()
+		if err != nil {
+			return nil, err
+		}
+
+	default:
+		return nil, errors.New("invalid env")
+	}
+
+	return &Logger{logger}, nil
+}
